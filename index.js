9bc4e47cf4b100aac07bc6394c7febe3cc0a51a0
Mehak-Akram <161160706+Mehak-Akram@users.noreply.github.com>
2024-03-15 15:52:05 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+// Example 1 string 
+var a = "Hello";
+console.log(a);
+//Example 2 number
+var age = 30;
+console.log(age);
+//Example 3 boolean
+var ispass = true;
+console.log(ispass);
+//Example 4 null
+var nullvalue = null;
+console.log(nullvalue);
+// Example 5 symbol (@ # $ &)
+//Example 6 bignit (it is used to store big integer values that are to big to represented by a normal js value)
+//camelCase
+//snake_case
+//PascalCase
+var userName = 'fatima'; //camelCase
+var user_name = 'mehak'; //snake_case
+var UserName = 'ayesha'; //PascalCase
+console.log(userName, user_name, UserName);
---
+++ index.ts
@@ -0,0 +1,36 @@
+// Example 1 string 
+
+let a: string = "Hello";
+console.log(a);
+
+//Example 2 number
+
+let age: number = 30;
+console.log(age)
+
+//Example 3 boolean
+
+let ispass: boolean = true;
+console.log(ispass);
+
+
+//Example 4 null
+
+let nullvalue: null = null;
+console.log (nullvalue);
+
+// Example 5 symbol (@ # $ &)
+
+//Example 6 bignit (it is used to store big integer values that are to big to represented by a normal js value)
+
+
+//camelCase
+//snake_case
+//PascalCase
+
+let userName: string = 'fatima' //camelCase
+let user_name: string = 'mehak' //snake_case
+let UserName: string = 'ayesha' //PascalCase
+
+console.log(userName,user_name,UserName);
+
