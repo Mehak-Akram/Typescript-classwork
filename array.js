a7634ec847f22c83fedd799c9296e492ffc7be88
Mehak-Akram <161160706+Mehak-Akram@users.noreply.github.com>
2024-03-15 15:52:51 +0500

    Add files via upload

---
+++ array.js
@@ -0,0 +1,12 @@
+//Array []
+var students = ["fatima", "Mehak", "Ayesha", "Zainab"];
+console.log(students);
+console.log(students[2], students[3]);
+var numberArray = [23, 56, 78, 30,];
+console.log(numberArray);
+console.log(numberArray[1]);
+var booleanArray = [true, false, true, false];
+console.log(booleanArray);
+console.log(booleanArray[2]);
+var array = ["ayesha", 25, true];
+console.log(array);
---
+++ array.ts
@@ -0,0 +1,21 @@
+//Array []
+
+let students: string[]  = ["fatima","Mehak","Ayesha","Zainab"]
+console.log(students);
+console.log(students[2], students[3]);
+
+
+
+let numberArray: number[] = [23, 56, 78, 30,]
+console.log(numberArray);
+console.log(numberArray[1]);
+
+
+
+let booleanArray: boolean[] = [true, false, true, false]
+console.log(booleanArray);
+console.log(booleanArray[2]);
+
+let array: any[] = ["ayesha", 25, true]
+console.log(array); 
+
