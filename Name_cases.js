b9c4cbb53b125aa8f6cae591b56dd114f70259f4
Mehak-Akram <161160706+Mehak-Akram@users.noreply.github.com>
2024-05-06 23:44:52 +0500

    Delete Name_cases.js

--- Name_cases.js
+++
@@ -1,8 +0,0 @@
-//Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
-var personName = "mehak";
-var lowercase = personName.toLowerCase();
-var uppercase = personName.toUpperCase();
-var titlecase = personName.charAt(0).toUpperCase() + personName.slice(1).toLowerCase();
-console.log(lowercase);
-console.log(uppercase);
-console.log(titlecase);
